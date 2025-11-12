20ca52c52e468eb21cb842b72b4bd8450e6192f5
enos <enyamor@gmail.com>
2025-11-12 08:24:37 -0800

    the logo project is coming on good

--- turtle_lib6.py
+++ turtle_lib6.py
@@ -8,77 +8,82 @@ def law_of_cosines(angle, b):
     angle_otra = (180 - angle)/2
     print('different side : ' , dif_side)
     print("Other angles each: ", angle_otra)
-    return dif_side
+    return dif_side, angle_otra
 
 if __name__ == "__main__":
     ang = float(input("Enter Angle :"))
     side = float(input("Enter Symetric side :"))
-    law_of_cosines(ang, side)
-
-
-
-'''
-
+    uniq_side, sim_angles =law_of_cosines(ang, side)
+    print(uniq_side)
+    print(sim_angles)
+    
 #to have the library and its modules and classes available
-screen = turtle.Screen()
-screen.bgcolor("orange")
-screen.title("cursor art")
+    screen = turtle.Screen()
+    screen.bgcolor("orange")
+    screen.title("cursor art")
 
 
-cursor = turtle.Turtle()
-cursor.color()
-cursor.speed(20)
-#cursor.penup()
-fw1 = 12
-fw2 = 13
-fw3 = 5
-fw4 = 24
-fw5 = 10
+    cursor = turtle.Turtle()
+    cursor.color()
+    cursor.speed(10)
+    #cursor.penup()
+
+    cursor.left(sim_angles)
+    cursor.forward(side)
+    cursor.left(180 - sim_angles)
+    cursor.forward(uniq_side)
+    cursor.left(90 - sim_angles)
+    cursor.forward(side)
 
 
-for i in range(100): 
-    
-    cursor.forward(fw1)
-    fw1*=2
-    cursor.left(157.4)
-    cursor.forward(fw2)
-    fw1*=2
-    cursor.left(112.6)
-    cursor.forward(fw3)
-    fw3*=2
 
-    cursor.right(90)
-    cursor.forward(fw1)
-    fw1*=2
-    cursor.right(157.4)
-    cursor.forward(fw2)
-    fw2*=2
-    cursor.right(112.6)
-    cursor.forward(fw5)
-    fw5*=2
-    cursor.left(112.6)
-    cursor.forward(fw2)
-    fw2*=2
-    cursor.left(157.4)
-    cursor.forward(fw4)
-    fw4*=2
-    cursor.left(157.4)
-    cursor.forward(fw2)
-    fw2*=2
 
-   # fw1= fw1*2
-    #fw2= fw2*2
-    
+    '''
+    for i in range(100): 
+        
+        cursor.forward(fw1)
+        fw1*=2
+        cursor.left(157.4)
+        cursor.forward(fw2)
+        fw1*=2
+        cursor.left(112.6)
+        cursor.forward(fw3)
+        fw3*=2
+
+        cursor.right(90)
+        cursor.forward(fw1)
+        fw1*=2
+        cursor.right(157.4)
+        cursor.forward(fw2)
+        fw2*=2
+        cursor.right(112.6)
+        cursor.forward(fw5)
+        fw5*=2
+        cursor.left(112.6)
+        cursor.forward(fw2)
+        fw2*=2
+        cursor.left(157.4)
+        cursor.forward(fw4)
+        fw4*=2
+        cursor.left(157.4)
+        cursor.forward(fw2)
+        fw2*=2
+
+    # fw1= fw1*2
+        #fw2= fw2*2
+        
+
+
+
+        cursor.forward(10)
+        cursor.left()
+        cursor.forward(24)
+        cursor.left()
+        cursor.forward(26)
+        
     '''
 
-'''
-    cursor.forward(10)
-    cursor.left()
-    cursor.forward(24)
-    cursor.left()
-    cursor.forward(26)'''
 
 
-
-#cursor.pendown
-#screen.exitonclick()
+    #cursor.pendown
+    screen.exitonclick()
