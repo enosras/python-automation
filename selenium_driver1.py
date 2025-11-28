53077362d6eb54637555ee5809a638f609d94c44
enos <enyamor@gmail.com>
2025-11-28 08:22:12 -0800

    Revised doc-string for selenium .get and .Browser drivers

--- selenium_driver1.py
+++ selenium_driver1.py
@@ -1,12 +1,23 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Safari('./safaridriver')
-
-driver.get("http://Users/enos/phptest/phpmysql.html")
+#driver = webdriver.Safari('./safaridriver')
 
 
+driver = webdriver.Firefox()
+url = "http://127.0.0.1:8080"
+x = driver.get(url)
+
+print(x)
+#help(driver)
+
+#print(driver.get.__doc__)
+#driver.get("http://Users/enos/phptest/phpmysql.html")
+
+'''
 print("testing")
 time.sleep(10)
 driver.quit()
 #sleep(10)
+
+'''
