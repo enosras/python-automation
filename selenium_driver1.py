2b3fbe7490d2be735561771c718c3fcb0427316a
enos <enyamor@gmail.com>
2025-12-06 07:38:44 -0800

    slenium log and metrics cookie extraction

--- selenium_driver1.py
+++ selenium_driver1.py
@@ -3,8 +3,11 @@ import time
 import requests
 
 
-cookies_dict = {}
-url = "https://www.google.com"
+#cookies_dict = {}
+#used this link for testing cookies retrieval and other selenium functions
+#chose this url because it is my alma mater site and I am familiar with it
+
+url = "https://www.usiu.ac.ke"
 #url = "http://localhost:8080"
 driver = webdriver.Safari('./safaridriver')
 
@@ -27,9 +30,28 @@ time.sleep(5)
 
 #help(driver.get_cookies()) 
 #response = requests.get(url, cookies=cookies_dict)
+cookies_dict = {cookie['name']: cookie['value'] for cookie in driver.get_cookies()}
+print("Cookies Dict:", cookies_dict)
 print("Cookies:", driver.get_cookies())
 
+with open('cookies.txt', 'w') as f:
+    for cookie in driver.get_cookies():
+        f.write(f"{cookie}\n")
+
+#capturing only specific cookie
+with open('specific_cookie.txt', 'w') as f:
+    
+        x = 0
+        for i in cookies_dict:
+            f.write(f"item {x} {i}\n")
+            x += 1
+
+
+
+# Testing page refresh
 driver.refresh()
+
+
 time.sleep(5)
 driver.quit()
 
