94e06874a3195dc5e2e2e261f8dce08abb729d07
enos <enyamor@gmail.com>
2025-12-09 11:09:47 -0800

    more on custom CLI

--- typer_lib.py
+++ typer_lib.py
@@ -4,15 +4,19 @@ import subprocess
 app =typer.Typer()
 #k = app.__doc__
 
-@app.command()
-def jobs(task:str):
-    typer.echo(f"Running task: {task}")
+@app.command("job")
+def jobs(job:str):
+    subprocess.run("ps")
+    #typer.echo(f"Running task: {task}")
+@app.command("qr")
+def jobs(qr:str, link:str = typer.Argument):
+    qrcommand = ["qrencode",  "-o", "test.png", link ]
+    subprocess.run(qrcommand)
 
-@app.command()
+@app.command("user")
 def users(user:str):
-    #output = subprocess("finger")
-    typer.echo(f"{user}")
-
+    subprocess.run("whoami")
+    #typer.echo(f"output for cmds user and option {user}")
 if __name__ == "__main__":
     app()
     #print(k)
