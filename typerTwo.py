f2943e8d65a64efe8b6a98e283b31837632d2333
enos <enyamor@gmail.com>
2025-12-09 17:30:13 -0800

    automation procedures for devops

--- typerTwo.py
+++ typerTwo.py
@@ -1,5 +1,15 @@
+import typer
+import string
+
 def typerTwo( name: str ):
-    print(f"Welcome Home : {name}")
+
+    naMe = name.capitalize()
+    print(f"Welcome Home : {naMe}")
+
+
+if __name__ == "__main__":
+    namae = input("Enter Name : ")
+    typerTwo(namae)
 
 
 #use typer ./typerTwo.py run enos     to test 
