dbbb01fd0be798ed15de6e4aa99db0e57a082886
enos <enyamor@gmail.com>
2025-12-17 10:09:09 -0800

    advance to custom random facts setup

--- rand_facts.py
+++ rand_facts.py
@@ -1,13 +1,19 @@
 import randfacts
+import typer
+import random
+
 
 '''
 randwand = randfacts.__doc__
 print(randwand)
-'''
+
+randwand = randfacts.get_fact()
+print(randwand)
 
 randwanda = randfacts.get_fact(only_unsafe=True)
 print(randwanda)
+'''
+#if = random.__doc__
+#print(f)
 
-
-randwand = randfacts.unsafe_facts()
-print(randwand)
+help(random)
