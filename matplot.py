bf47ee4e042a9c1c390b14596897e880ed9ce48a
enos <enyamor@gmail.com>
2026-06-12 17:40:52 -0700

    touches on tui tooling skills

--- matplot.py
+++ matplot.py
@@ -1,24 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
-# import sele
 #
-# 1. Prepare data
-# Using numpy to create an array of x-points
-xpoints = np.array([1, 2, 6, 8])
-# Corresponding y-points
-ypoints = np.array([3, 8, 1, 10])
+# # import sele
+# #
+# # 1. Prepare data
+# # Using numpy to create an array of x-points
+# xpoints = np.array([1, 2, 6, 8])
+# # Corresponding y-points
+# ypoints = np.array([3, 8, 1, 10])
+#
+# # 2. Create the plot
+# plt.plot(xpoints, ypoints)
+#
+# # 3. Add labels and a title (optional customization)
+# plt.xlabel("X-axis Label")
+# plt.ylabel("Y-axis Label")
+# plt.title("Simple Line Plot")
+#
+# # 4. Display the plot
+# plt.show()
 
-# 2. Create the plot
-plt.plot(xpoints, ypoints)
-
-# 3. Add labels and a title (optional customization)
-plt.xlabel("X-axis Label")
-plt.ylabel("Y-axis Label")
-plt.title("Simple Line Plot")
-
-# 4. Display the plot
-plt.show()
-
-# print(f"NumPy Version: {np.__version__}")
-# print(f"Matplotlib Version: {plt.matplotlib.__version__}")
+print(f"NumPy Version: {np.__version__}")
+print(f"Matplotlib Version: {plt.matplotlib.__version__}")
--- rich_lib.py
+++ rich_lib.py
@@ -1,15 +1,21 @@
-#rich is equivalent of eza or glow, makes your output pretty
+# rich is equivalent of eza or glow, makes your output pretty
 
 import subprocess
+
 from rich import print
 from rich.progress import track
-#for task in track(range(1), description="Processing.."):
+
+# for task in track(range(1), description="Processing.."):
 for task in track(range(1), description="Processing.."):
-    result = subprocess.run(["tree"], capture_output=True, text=True, check=True)
+    result = subprocess.run(
+        ["hyperfine", "-h"], capture_output=True, text=True, check=True
+    )
     print("Stdout:", result.stdout)
     print("Stderr:", result.stderr)
 
     # Execute a command with shell=True (use with caution for security)
     # This allows the shell to interpret the command string
-    result_shell = subprocess.run("echo 'Hello from shell!'", shell=True, capture_output=True, text=True)
+    result_shell = subprocess.run(
+        "echo 'Hello from shell!'", shell=True, capture_output=True, text=True
+    )
     print("Shell output:", result_shell.stdout)
